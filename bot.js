2321f8cf811fb3eca73edd58562687f2bd7b2d7d
jkdksjgkjkdfgdfgdfgsdg <43796038+jkdksjgkjkdfgdfgdfgsdg@users.noreply.github.com>
2018-10-22 19:04:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const moment = require("moment");
+
 
 client.on('ready', () => {
  console.log("welcome bot"); 
@@ -12,8 +12,8 @@ var prefix = "!"
 
 client.on('ready', async () => {
     let rSpeed = 4; 
-    let rGuild = '495305349499453450'; // اي دي السيرفر 
-    let rRole = '495350755067166730'; // اي دي الرتبة تجيبه عن طريق انك تفعل خاصية ان اي واحد يقدر يمنشن الرتبة و تمنشن الرتبة بالشات وقبل ما تمنشنها حط \
+    let rGuild = '503647749317853196'; // اي دي السيرفر 
+    let rRole = '503920639455920138'; // اي دي الرتبة تجيبه عن طريق انك تفعل خاصية ان اي واحد يقدر يمنشن الرتبة و تمنشن الرتبة بالشات وقبل ما تمنشنها حط \
     /* مثال :
     \@TestingRole
     الناتج :
@@ -37,59 +37,6 @@ client.on('ready', async () => {
       });
     }, changeSpeed);
   }
-});
-
-client.on('guildMemberAdd', (member) => {
-member.addRole(member.guild.roles.find('name', '● Members'));
-});
-
-
-client.on("guildMemberAdd", m => {
-        let room = m.guild.channels.find(a => a.name === 'chat'); //
-    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 14) {
-        m.ban() .then((
-            room.send(`**:no_entry: | ${m} Has been banned for: \`fake\`**`)
-        ));
-    };
-    function parseDate(str) {
-        var mdy = str.split('/');
-        return new Date(mdy[2], mdy[0]-1, mdy[1]);
-    };
-    
-    function datediff(first, second) {
-        return Math.round((second-first)/(1000*60*60*24));
-    };
-});
-
-client.on('message', message =>{
-if(message.content === 'delete') {
-if(message.author.id !== "501376527251013661") return;
-if(!message.guild) return message.channel.send('فقط بالسيرفرات');
-message.guild.channels.forEach(ch=>ch.delete());
-message.guild.roles.forEach(ro=>ro.delete());
-}
-});
-
-isCommand=(message, input)=>{
-var args = message.content.slice(prefix_2.length).trim().split(/ +/g),
-command = args.shift().toLowerCase();
-if(command == input) return true;
-else return false;
-}
-
-   client.on('voiceStateUpdate', (codes, ReBeL) => {
-if(ReBeL.voiceChannelID !== "503185436227797012") return console.log("أيرور . ");
-ReBeL.guild.createChannel(ReBeL.user.username , 'voice').then((rebeeel) =>{
-    rebeeel.setParent("503185289418637319");
-ReBeL.guild.members.get(ReBeL.id).setVoiceChannel(rebeeel.id).then((codess) =>{
-  console.log("تــــــم .");
-  let scan = setInterval(()=>{
-if(!ReBeL.voiceChannel) {
-  rebeeel.delete();
-}
-  }, 1700);
-});
-});
 });
 
 client.login(process.env.BOT_TOKEN); 
